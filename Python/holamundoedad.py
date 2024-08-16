657af08cdc9adc68958d72a35dde58870c265d37
Mikaela28HS <aleshka.lujan@ucb.edu.bo>
2024-08-16 10:37:04 -0400

    Create holamundoedad.py
    codigo_holamundoedad

---
+++ Python/holamundoedad.py
@@ -0,0 +1,15 @@
+def hola_mundo():
+    print("HOLA MUNDO!")
+
+def Obtener_Edad():
+    edad = input("Inserte su edad: ")
+    print("Su edad es: ", edad)
+
+def Calcular_Edad():
+    anioNacimiento = int(input("Ingrese el año que nacio: "))
+    edad = 2024 - anioNacimiento
+    print("Su edad es: ", edad)
+
+hola_mundo()
+Obtener_Edad()
+Calcular_Edad()
