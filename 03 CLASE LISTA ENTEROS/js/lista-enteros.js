db6e5b38e31b9170a6ef49acb17f8336fd2ea684
Mikaela28HS <aleshka.lujan@ucb.edu.bo>
2024-10-04 10:09:20 -0400

    .

--- 03 CLASE LISTA ENTEROS/js/lista-enteros.js
+++ 03 CLASE LISTA ENTEROS/js/lista-enteros.js
@@ -39,6 +39,71 @@ class ListaEnteros {
 }
 
 const miLista = new ListaEnteros();
+const listaEnteros = new ListaEnteros();
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista); // Imprime: [21, 12, 15, 30]
+
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+//RETORNAR EL ELEMENTO MENOR DE LA LISTA, dado que la lista solo almacena valores positivos
+
+// var datoMenor = -1;
+
+// //-----------------------------------------------------------
+// var a = listaEnteros.lista[0];
+// if(a > datoMenor){
+//     datoMenor = a;
+// }
+// console.log("Dato Menor: " + datoMenor);
+// //-----------------------------------------------------------
+// var b = listaEnteros.lista[1];
+// if(a > b){
+//     datoMenor = b;
+// }
+// console.log("Dato Menor: " + datoMenor);
+// //-----------------------------------------------------------
+// var c = listaEnteros.lista[2];
+// if(b > c){
+//     datoMenor = c;
+// }
+// console.log("Dato Menor: " + datoMenor);
+// //-----------------------------------------------------------
+// var d = listaEnteros.lista[3];
+// if(c > d){
+//     datoMenor = d;
+// }
+// console.log("Dato Menor: " + datoMenor);
+
+let datoMenor = -1;
+
+for (let i = 0; i < listaEnteros.length; i++) {
+    if (i === 0 || listaEnteros[i - 1] > listaEnteros[i]) {
+        datoMenor = listaEnteros[i];
+    }
+
+}
+console.log("Dato Menor: " + datoMenor);
+
+var datoMayor = listaEnteros.lista[0];
+
+for (var i = 1; i < listaEnteros.lista.length; i++) {
+    datoMayor = datoMayor > listaEnteros.lista[i] ? datoMayor : listaEnteros.lista[i];
+}
+
+console.log("Dato Mayor: " + datoMayor);
 
 document.getElementById('agregarBtn').addEventListener('click', () => {
     const valor = parseInt(document.getElementById('numero').value);
